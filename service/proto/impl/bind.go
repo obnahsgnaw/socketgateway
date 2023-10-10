6792b6d35e51bee88d864705838b1b3e0986d378
wangsb <wangsb@efly-cetc.com>
2023-10-10 10:24:38 +0800

    perf(proto impl): perf grpc impl err status

--- service/proto/impl/bind.go
+++ service/proto/impl/bind.go
@@ -2,9 +2,10 @@ package impl
 
 import (
 	"context"
-	"errors"
 	bindv1 "github.com/obnahsgnaw/socketapi/gen/bind/v1"
 	"github.com/obnahsgnaw/socketgateway/pkg/socket"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type BindService struct {
@@ -18,56 +19,57 @@ func NewBindService(s func() *socket.Server) *BindService {
 	}
 }
 
-func (gw *BindService) BindId(_ context.Context, in *bindv1.BindIdRequest) (_ *bindv1.BindIdResponse, err error) {
+func (gw *BindService) BindId(_ context.Context, in *bindv1.BindIdRequest) (resp *bindv1.BindIdResponse, err error) {
 	if in.Fd == 0 {
-		err = errors.New("fd is required")
+		err = status.New(codes.InvalidArgument, "param:Fd is required").Err()
 		return
 	}
 	if in.Id == "" {
-		err = errors.New("id is required")
+		err = status.New(codes.InvalidArgument, "param:Id is required").Err()
 		return
 	}
 	if in.IdType == "" {
-		err = errors.New("type is required")
+		err = status.New(codes.InvalidArgument, "param:IdType is required").Err()
 		return
 	}
 	conn := gw.s().GetFdConn(int(in.Fd))
 	if conn == nil {
-		err = errors.New("fd conn not exists")
+		err = status.New(codes.NotFound, "connection not found").Err()
 		return
 	}
 	gw.s().BindId(conn, socket.ConnId{
 		Id:   in.Id,
 		Type: in.IdType,
 	})
+	resp = &bindv1.BindIdResponse{}
 	return
 }
 
-func (gw *BindService) BindExist(_ context.Context, in *bindv1.BindExistRequest) (p *bindv1.BindExistResponse, err error) {
+func (gw *BindService) BindExist(_ context.Context, in *bindv1.BindExistRequest) (resp *bindv1.BindExistResponse, err error) {
 	if in.Id == "" {
-		err = errors.New("id is required")
+		err = status.New(codes.InvalidArgument, "param:Id is required").Err()
 		return
 	}
 	if in.IdType == "" {
-		err = errors.New("type is required")
+		err = status.New(codes.InvalidArgument, "param:IdType is required").Err()
 		return
 	}
 	conn := gw.s().GetIdConn(socket.ConnId{
 		Id:   in.Id,
 		Type: in.IdType,
 	})
-	p.Exist = conn == nil
+	resp.Exist = conn == nil
 	return
 }
 
 func (gw *BindService) UnBindId(_ context.Context, in *bindv1.UnBindIdRequest) (resp *bindv1.UnBindIdResponse, err error) {
 	if in.Fd == 0 {
-		err = errors.New("fd is required")
+		err = status.New(codes.InvalidArgument, "param:fd is required").Err()
 		return
 	}
 	conn := gw.s().GetFdConn(int(in.Fd))
 	if conn == nil {
-		err = errors.New("fd conn not exists")
+		err = status.New(codes.NotFound, "connection not found").Err()
 		return
 	}
 	gw.s().UnbindId(conn, socket.ConnId{
