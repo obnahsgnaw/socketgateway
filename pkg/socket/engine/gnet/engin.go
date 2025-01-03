f01142e131c4070ff76beb341938392d34525aba
obnahsgnaw <obnahsgnaw@qq.com>
2025-01-03 17:47:51 +0800

    fix(udp): x

--- pkg/socket/engine/gnet/engin.go
+++ pkg/socket/engine/gnet/engin.go
@@ -49,7 +49,7 @@ func (e *Engine) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
 			e.OnClose(c, nil)
 		}))
 	} else {
-		c1 = newConn(c, connCtx)
+		c1 = newConn(c, connCtx) // TODO fix close for server event
 	}
 	e.connections.Store(c.Fd(), c1)
 	e.event.OnOpen(e.server, c1)
